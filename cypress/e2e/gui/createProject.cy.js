0ebe57ff9165fab36db6cecab58dd14426aef773
Suelen Francisco Marques <59808425+Suelen-Francisco-Marqes@users.noreply.github.com>
2023-01-31 21:56:14 -0300

    Testando criação de issue via API

--- cypress/e2e/gui/createProject.cy.js
+++ cypress/e2e/gui/createProject.cy.js
@@ -1,6 +1,7 @@
 import { faker } from '@faker-js/faker'
 
-describe('Create Project', () => {
+const options = { env: { snapshotOnly: true } }
+describe('Create Project',options, () => {
   beforeEach(() => {
     cy.login()
   })
